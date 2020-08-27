7066ac2a10fa2894b5943e123f87544353c417f5
rahulsai1999 <noneuser2183@gmail.com>
2020-08-27 11:22:20 +0530

    key changes for docker repo

--- install_docker.sh
+++ install_docker.sh
@@ -3,8 +3,8 @@
 echo "Updating Repositories"
 sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -y | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg  | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update -y
 sudo apt-cache policy docker-ce -y
 sudo apt install docker-ce -y
