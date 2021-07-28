569f5a6d79be65c0dfdc223fa5ef64e8971b7546
Rahul Sai <rs.rahulsai23@gmail.com>
2021-07-28 14:54:03 +0530

    docker changes

--- install_docker.sh
+++ install_docker.sh
@@ -11,3 +11,10 @@ sudo apt-cache policy docker-ce -y
 sudo apt install docker-ce -y
 sudo docker -v
 echo "Docker Installed"
+echo "Installing Docker-Compose"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
+echo "Docker-Compose installed"
