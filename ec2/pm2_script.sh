78e47a8234fc28712a18fcd63aba369ddf22fa2e
Rahul Sai <rs.rahulsai23@gmail.com>
2021-09-07 14:57:32 +0530

    updated PM2 script

--- ec2/pm2_script.sh
+++ ec2/pm2_script.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 
-export NODE_ENV=production && export PORT=5000 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5001 && pm2 start app.js -f --name backend 
-export NODE_ENV=production && export PORT=5002 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5003 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5004 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5005 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5006 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5007 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5008 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5009 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5010 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5011 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5012 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5013 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5014 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5015 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5016 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5017 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5018 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5019 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5020 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5021 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5022 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5023 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5024 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5025 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5026 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5027 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5028 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5029 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5030 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5031 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5032 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5033 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5034 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5035 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5036 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5037 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5038 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5039 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5040 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5041 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5042 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5043 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5044 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5045 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5046 && pm2 start app.js -f --name backend 
-export NODE_ENV=production && export PORT=5047 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5048 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5049 && pm2 start app.js -f --name backend
-export NODE_ENV=production && export PORT=5050 && pm2 start app.js -f --name backend
+export NODE_ENV=production && export PORT=5000 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5001 && pm2 start npm --name backend -- start -f 
+export NODE_ENV=production && export PORT=5002 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5003 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5004 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5005 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5006 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5007 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5008 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5009 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5010 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5011 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5012 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5013 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5014 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5015 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5016 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5017 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5018 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5019 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5020 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5021 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5022 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5023 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5024 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5025 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5026 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5027 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5028 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5029 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5030 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5031 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5032 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5033 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5034 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5035 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5036 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5037 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5038 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5039 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5040 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5041 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5042 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5043 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5044 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5045 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5046 && pm2 start npm --name backend -- start -f 
+export NODE_ENV=production && export PORT=5047 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5048 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5049 && pm2 start npm --name backend -- start -f
+export NODE_ENV=production && export PORT=5050 && pm2 start npm --name backend -- start -f
